ab32555ae17ab7d21ae5e17c330231f00b4c5267
bbxyard <bbxyard@gmail.com>
2015-04-02 01:14:52 +0800

    add command demo

--- src/main/java/com/bbxyard/tconv/App.java
+++ src/main/java/com/bbxyard/tconv/App.java
@@ -1,13 +1,40 @@
 package com.bbxyard.tconv;
 
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+
 /**
  * Hello world!
  *
  */
 public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+{    
+	public static void main(String[] args) throws Exception {
+		// Create a Parser
+		CommandLineParser parser = new BasicParser();
+		Options options = new Options();
+		options.addOption("h", "help", false, "Print this usage information");
+		options.addOption("v", "verbose", false,
+				"Print out VERBOSE information");
+		options.addOption("f", "file", true, "File to save program output to");
+		System.out.println(options);
+		// Parse the program arguments
+		CommandLine commandLine = parser.parse(options, args);
+		// Set the appropriate variables based on supplied options
+		boolean verbose = false;
+		String file = "";
+
+		if (commandLine.hasOption('h')) {
+			System.out.println("Help Message");
+			System.exit(0);
+		}
+		if (commandLine.hasOption('v')) {
+			verbose = true;
+		}
+		if (commandLine.hasOption('f')) {
+			file = commandLine.getOptionValue('f');
+		}
+	}
 }
